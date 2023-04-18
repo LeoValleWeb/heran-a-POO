c0860c7d9482c75c8470a4af0732af0bb4ce3027
LeoValleWeb <leomartinsvalle@gmail.com>
2023-04-18 14:36:00 +0000

    save3

--- Principal.java
+++ Principal.java
@@ -1,3 +1,10 @@
 public class Principal {
     
+    public static void main(String[] args){
+        Figura figura1 = new Circulo("vermelho", 5);
+        Figura figura2 = new Retangulo("roxo", 3, 4);
+        
+        System.out.println("A área círculo é: "+ figura1);
+        System.out.println("A área retangulo é: "+ figura2);
+    }
 }
